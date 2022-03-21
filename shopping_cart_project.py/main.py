a5aeb335f2e92c1176824519b4c7c49b3097b094
Michael J <johnson_michael1329@yahoo.com>
2022-03-21 11:44:46 -0500

    created the customers info function in the main.py file that takes the customers name and assign its own values accordingly.

--- shopping_cart_project.py/main.py
+++ shopping_cart_project.py/main.py
@@ -1,18 +1,26 @@
 from customer import Customer
-from product import Product
 
-shopper1 = Customer("Michael")
+def customers_info(name):
+  customers_details = Customer(name)
+  customers_name = customers_details.name
 
-print(shopper1.name)
-print("-------------------")
-print(shopper1.cart.add_to_cart())
-print("-------------------")
-print(shopper1.cart.add_to_cart())
-print("-------------------")
-print(shopper1.cart.current_items)
-print("-------------------")
-print(f'Your total is ${shopper1.cart.cart_total()}')
-print("-------------------")
-shopper1.cart.empty_cart()
-print(shopper1.cart.current_items)
-print("-------------------")
+  print(f'{customers_name}')
+  print("-------------------")
+  print(customers_details.cart.add_to_cart())
+  print("-------------------")
+  print(customers_details.cart.add_to_cart())
+  print("-------------------")
+  print(f'Hi {customers_name} here are your items {customers_details.cart.current_items}')
+  print("-------------------")
+  print(f'Your total is ${customers_details.cart.cart_total()}')
+  print("-------------------")
+# customers_details.cart.empty_cart()
+  print(customers_details.cart.current_items)
+  print("-------------------")
+  return customers_details
+
+
+jimme = customers_info("Jimme")
+henry = customers_info("Henry")
+alfred = customers_info("Alfred")
+
